d658df712e6977be79e3147b4e70d2101c080e45
Arturs Demiters <arturs@codecraft.lv>
2019-04-24 02:14:55 +0300

    chore(lint): rule change

--- .eslintrc.js
+++ .eslintrc.js
@@ -99,7 +99,7 @@ module.exports = {
     // ESLint plugin for prettier formatting
     // https://github.com/prettier/eslint-plugin-prettier
     'prettier/prettier': 'error',
-    'react/destructuring-assignment': 'never',
+    'react/destructuring-assignment': 'always',
   },
 
   settings: {
