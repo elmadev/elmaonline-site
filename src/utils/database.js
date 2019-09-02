959c6f5001e7a66333441357cb987603bbce0909
Sune Høyer Sørensen <sune@headfitted.dk>
2019-09-02 20:17:59 +0200

    feat(db): query benchmarking

---
+++ src/utils/database.js
@@ -0,0 +1,19 @@
+import fs from 'fs';
+import moment from 'moment';
+
+export function log(func, query, benchmark) {
+  fs.appendFile(
+    `./events/${moment().format('YYYY-MM-DD')}.log`,
+    `${func}: ${benchmark} ${query}\r\n`,
+  );
+  if (benchmark > 1000) {
+    fs.appendFile(
+      `./events/${moment().format('YYYY-MM-DD')}-slow.log`,
+      `${func}: ${benchmark} ${query}\r\n`,
+    );
+  }
+}
+
+export function dummy() {
+  return true;
+}
