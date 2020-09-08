6fd74e4f3954d53d33392389ab8eb360e5d256dc
Pablo Corso <pabcorso@gmail.com>
2020-09-08 21:42:28 +0200

    refactor: 💡 Use date-fns to format logger output

--- src/utils/discord/logger.js
+++ src/utils/discord/logger.js
@@ -1,27 +1,10 @@
 const fs = require('fs');
-
-const padZeros = (value, length = 2) => {
-  return value ? value.toString().padStart(length, '0') : '';
-};
-
-const formatDate = date => {
-  const month = padZeros(date.getMonth() + 1);
-  const day = padZeros(date.getDate());
-  const year = date.getFullYear();
-  return [year, month, day].join('-');
-};
-
-const formatTime = date => {
-  const hours = padZeros(date.getHours());
-  const minutes = padZeros(date.getMinutes());
-  const seconds = padZeros(date.getSeconds());
-  return [hours, minutes, seconds].join(':');
-};
+const { format } = require('date-fns');
 
 const log = ({ userName, userId, action, message, stack }) => {
   const dateNow = new Date();
-  const date = formatDate(dateNow);
-  const time = formatTime(dateNow);
+  const date = format(dateNow, 'yyyy-MM-dd');
+  const time = format(dateNow, 'HH:mm:ss');
   const stackMessage = stack ? `STACK: ${stack}` : '';
   fs.appendFile(
     `./bn/${date}.log`,
