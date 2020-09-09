f4573f5e7835015683f42cbb07f43ebbddb11bcd
Pablo Corso <pabcorso@gmail.com>
2020-09-09 20:34:37 +0200

    refactor: 💡 Refactor bnStore and add meta data

---
+++ src/utils/discord/battleNotifier/bnStore/bnStore.js
@@ -0,0 +1,36 @@
+const { writeJsonFile, readJsonFile } = require('./jsonFs');
+
+const createBnStore = path => {
+  const getAll = async () => {
+    return readJsonFile(path);
+  };
+
+  const get = async userId => {
+    const bnStore = await getAll();
+    return bnStore[userId];
+  };
+
+  const set = async (userId, values) => {
+    const bnStore = await getAll();
+
+    const updatedAt = new Date().toISOString();
+    const newConfig = {
+      createdAt: updatedAt,
+      ...bnStore[userId],
+      updatedAt,
+      ...values,
+    };
+
+    const data = { ...bnStore, [userId]: newConfig };
+    await writeJsonFile(path, data);
+  };
+
+  return {
+    get,
+    set,
+    getAll,
+    path,
+  };
+};
+
+module.exports = createBnStore;
