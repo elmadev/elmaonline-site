65f64510849b7a552e2057ddfd39aca373ea3afd
Sune Høyer Sørensen <sune@headfitted.dk>
2020-09-11 22:34:59 +0200

    fix(log): save db log in persistant folder

--- src/utils/database.js
+++ src/utils/database.js
@@ -2,18 +2,28 @@ import fs from 'fs';
 import moment from 'moment';
 
 export function log(func, query, benchmark) {
-  fs.appendFile(
-    `./events/${moment().format('YYYY-MM-DD')}.log`,
-    `${func}: ${benchmark} ${query}\r\n`,
-    () => {},
-  );
-  if (benchmark > 1000) {
-    fs.appendFile(
-      `./events/${moment().format('YYYY-MM-DD')}-slow.log`,
-      `${func}: ${benchmark} ${query}\r\n`,
-      () => {},
-    );
+  let folder = './events/dblog/';
+  if (process.env.NODE_ENV === 'production') {
+    folder = '../events/dblog/';
   }
+  fs.mkdir(folder, { recursive: true }, err => {
+    if (!err) {
+      if (process.env.NODE_ENV === 'development') {
+        fs.appendFile(
+          `${folder}${moment().format('YYYY-MM-DD')}.log`,
+          `${func}: ${benchmark} ${query}\r\n`,
+          () => {},
+        );
+      }
+      if (benchmark > 1000) {
+        fs.appendFile(
+          `${folder}${moment().format('YYYY-MM-DD')}-slow.log`,
+          `${func}: ${benchmark} ${query}\r\n`,
+          () => {},
+        );
+      }
+    }
+  });
 }
 
 export function like(text) {
