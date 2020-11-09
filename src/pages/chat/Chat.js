17979932bdc9bc7dd4cb911a789002554af6220d
Arturs Demiters <arturs@codecraft.lv>
2020-11-09 19:29:28 +0200

    fix(other): use react-window to improve kuskilist performance

--- src/pages/chat/Chat.js
+++ src/pages/chat/Chat.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, useMemo } from 'react';
+import { VariableSizeList } from 'react-window';
 import { useStoreState, useStoreActions } from 'easy-peasy';
 import { useDebounce } from 'use-debounce';
 import queryString from 'query-string';
@@ -9,8 +10,11 @@ import {
   Typography,
   Grid,
   Switch,
+  ListSubheader,
   CircularProgress,
+  useMediaQuery,
 } from '@material-ui/core';
+import { useTheme, makeStyles } from '@material-ui/core/styles';
 import Autocomplete from '@material-ui/lab/Autocomplete';
 
 import ChatView from 'components/ChatView';
@@ -19,6 +23,103 @@ import Header from 'components/Header';
 import history from 'utils/history';
 
 import s from './Chat.css';
+
+const LISTBOX_PADDING = 8; // px
+
+function renderRow(props) {
+  const { data, index, style } = props;
+  return React.cloneElement(data[index], {
+    style: {
+      ...style,
+      top: style.top + LISTBOX_PADDING,
+    },
+  });
+}
+
+const OuterElementContext = React.createContext({});
+
+const OuterElementType = React.forwardRef((props, ref) => {
+  const outerProps = React.useContext(OuterElementContext);
+  // eslint-disable-next-line react/jsx-props-no-spreading
+  return <div ref={ref} {...props} {...outerProps} />;
+});
+
+function useResetCache(data) {
+  const ref = React.useRef(null);
+  React.useEffect(() => {
+    if (ref.current != null) {
+      ref.current.resetAfterIndex(0, true);
+    }
+  }, [data]);
+  return ref;
+}
+
+const useStyles = makeStyles({
+  listbox: {
+    boxSizing: 'border-box',
+    '& ul': {
+      padding: 0,
+      margin: 0,
+    },
+  },
+});
+
+// Adapter for react-window
+const ListboxComponent = React.forwardRef(function ListboxComponent(
+  props,
+  ref,
+) {
+  const { children, ...other } = props;
+  const itemData = React.Children.toArray(children);
+  const theme = useTheme();
+  const smUp = useMediaQuery(theme.breakpoints.up('sm'), { noSsr: true });
+  const itemCount = itemData.length;
+  const itemSize = smUp ? 36 : 48;
+
+  const getChildSize = child => {
+    if (React.isValidElement(child) && child.type === ListSubheader) {
+      return 48;
+    }
+
+    return itemSize;
+  };
+
+  const getHeight = () => {
+    if (itemCount > 8) {
+      return 8 * itemSize;
+    }
+    return itemData.map(getChildSize).reduce((a, b) => a + b, 0);
+  };
+
+  const gridRef = useResetCache(itemCount);
+
+  return (
+    <div ref={ref}>
+      <OuterElementContext.Provider value={other}>
+        <VariableSizeList
+          itemData={itemData}
+          height={getHeight() + 2 * LISTBOX_PADDING}
+          width="100%"
+          ref={gridRef}
+          outerElementType={OuterElementType}
+          innerElementType="ul"
+          itemSize={index => getChildSize(itemData[index])}
+          overscanCount={5}
+          itemCount={itemCount}
+        >
+          {renderRow}
+        </VariableSizeList>
+      </OuterElementContext.Provider>
+    </div>
+  );
+});
+
+const renderGroup = params => [
+  <ListSubheader key={params.key} component="div">
+    {params.group}
+  </ListSubheader>,
+  params.children,
+];
 
 const Chat = props => {
   const {
@@ -113,6 +214,8 @@ const Chat = props => {
     });
   };
 
+  const acClasses = useStyles();
+
   return (
     <div className={s.chat}>
       <Header h2>Chat Log Filter</Header>
@@ -147,6 +250,9 @@ const Chat = props => {
                 <Kuski kuskiData={option} flag team noLink />
               )}
               value={kuskiValue}
+              ListboxComponent={ListboxComponent}
+              renderGroup={renderGroup}
+              classes={acClasses}
             />
           ) : (
             <CircularProgress />
