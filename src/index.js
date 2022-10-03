49b9be00c95172701c173ab9290781d279404c82
Sune Høyer Sørensen <sunehs@gmail.com>
2022-10-03 17:29:02 +0200

    fix(config): static folder and missing files

--- src/index.js
+++ src/index.js
@@ -23,7 +23,7 @@ const app = express();
 // ----------------------------------------------------------
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../public')));
 
 // ----------------------------------------------------------
 // express middleware
