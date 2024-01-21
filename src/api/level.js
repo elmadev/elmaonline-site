f47bf4a6cd7c1051673623fa20d2e8fdefcc81cf
Ismo Metsävirta <ismo.metsavirta@gmail.com>
2024-01-21 13:02:44 +0200

    fix(levels): return empty array instead of null in get(Not)InIndexes

--- src/api/level.js
+++ src/api/level.js
@@ -258,7 +258,7 @@ const getLevels = async (
 
     // Find the intersection of arrays to use
     const intersectionResult =
-      arraysToIntersect.length > 0 ? intersection(...arraysToIntersect) : null;
+      arraysToIntersect.length > 0 ? intersection(...arraysToIntersect) : [];
 
     return intersectionResult;
   };
@@ -271,7 +271,7 @@ const getLevels = async (
 
     const arraysToConcat = arraysToUse.filter(array => array.length > 0);
 
-    return arraysToConcat.length > 0 ? [].concat(...arraysToConcat) : null;
+    return arraysToConcat.length > 0 ? [].concat(...arraysToConcat) : [];
   };
 
   const shouldIncludeInIndexes =
