31dc2d4e3298399a78bd6bf04cc2a291579d312f
GauChoob <starlight_tonight@hotmail.com>
2025-01-30 21:36:40 -0500

    docs(config): suggest 'local' for accessKeyId

--- src/config.local.js
+++ src/config.local.js
@@ -6,4 +6,6 @@ if (process.env.BROWSER) {
 
 // add keys from defaults.js that you want to change the value for
 // changes to this file will not be seen by git
-export default {};
+export default {
+  //accessKeyId: 'local', // for local testing of replay and lgr uploads without s3 access
+};
